4307751ebb7d373ce8fb00a8f4654a683b1b0350
Sinclair Target <sinclairtarget@gmail.com>
2019-05-06 14:54:50 -0500

    Add doit proposal tasks.

---
+++ dodo.py
@@ -0,0 +1,31 @@
+DOIT_CONFIG = {
+    'default_tasks': ['render_proposal']
+}
+
+def task_render_proposal():
+    """Generates HTML notebook output for proposal."""
+    target = 'proposal/proposal.html'
+    dep = 'proposal/proposal.ipynb'
+    return {
+        'file_dep': [dep],
+        'targets': [target],
+        'actions': [
+            f"jupyter nbconvert --execute --to html {dep}"
+        ],
+        'clean': True
+    }
+
+
+def task_sync_proposal():
+    """Generate .ipynb notebook from proposal markdown source."""
+    target = 'proposal/proposal.ipynb'
+    dep = 'proposal/proposal.md'
+    return {
+        'file_dep': [dep],
+        'targets': [target],
+        'actions': [
+            f"jupytext --to notebook {dep}",
+            f"jupytext --set-format ipynb,md --sync {target}"
+        ],
+        'clean': True
+    }
