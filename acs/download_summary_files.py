afc5b1bb2a44ba14565792dee87a813c80781be2
Sinclair Target <sinclairtarget@gmail.com>
2019-05-23 10:09:46 -0500

    Add script to download ACS summary files.

---
+++ acs/download_summary_files.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import subprocess
+import glob
+
+FILE_URL_TEMPLATE = ("https://www2.census.gov/programs-surveys/acs/"
+                     "summary_file/{}/data/5_year_by_state/"
+                     "Pennsylvania_Tracts_Block_Groups_Only.zip")
+
+YEARS = range(2009, 2013)
+
+def summary_file_url(year):
+    return FILE_URL_TEMPLATE.format(year)
+
+
+def year_dir(year):
+    return os.path.join(summary_file_dir, str(year))
+
+
+def year_zip_filename(year):
+    dirname = year_dir(year)
+    return os.path.join(dirname, 'summary_file.zip')
+
+
+def download_zip(year):
+    dirname = year_dir(year)
+    filename = year_zip_filename(year)
+    url = summary_file_url(year)
+    os.makedirs(dirname, exist_ok=True)
+
+    if not os.path.exists(filename):
+        subprocess.run(['curl', '-o', filename, url], check=True)
+
+    return filename
+
+
+def unzip(filename):
+    dirname = year_dir(year)
+    subprocess.run(['unzip', filename, '-d', dirname], check=True)
+    os.remove(filename)
+
+
+def clean(year):
+    dirname = year_dir(year)
+    margin_error_files = glob.glob(os.path.join(dirname, 'm*.txt'))
+    for f in margin_error_files:
+        os.remove(f)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        sys.stderr.write(f"usage: python {sys.argv[0]} "
+                          "<summary-file-directory>\n")
+        exit(1)
+
+    summary_file_dir = sys.argv[1]
+    if not os.path.exists(summary_file_dir):
+        sys.stderr.write(f"Path {summary_file_dir} does not exist!\n")
+        exit(1)
+
+    print('Downloading summary files. This might take a while...')
+    for year in YEARS:
+        zip_filename = download_zip(year)
+        unzip(zip_filename)
+        clean(year)
--- dodo.py
+++ dodo.py
@@ -1,3 +1,5 @@
+import os
+
 DOIT_CONFIG = {
     'default_tasks': ['render_proposal']
 }
@@ -31,8 +33,8 @@ def task_sync_proposal():
     }
 
 
-def task_fetch_acs_from_api():
-    """Fetches ACS data using the fetch from API script."""
+def task_acs_fetch():
+    """Fetches ACS data for 2013 to 2016 using the fetch from API script."""
     target = 'data/acs.csv'
     dep = 'acs/fetch_from_api.py'
     return {
@@ -40,5 +42,19 @@ def task_fetch_acs_from_api():
         'targets': [target],
         'actions': [
             f"python {dep} > {target}"
+        ]
+    }
+
+
+def task_acs_download():
+    """Downloads ACS summary files for 2009 to 2012 from Census website."""
+    target = os.path.abspath('./data/acs-summary-files')
+    dep = 'acs/download_summary_files.py'
+    return {
+        'file_dep': [dep],
+        'targets': [target],
+        'actions': [
+            f"mkdir -p {target}",
+            f"python {dep} {target}"
         ]
     }
