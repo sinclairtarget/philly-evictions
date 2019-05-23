21bd6cc4715930a9063da632afa74c1501c31e71
Sinclair Target <sinclairtarget@gmail.com>
2019-05-23 13:17:13 -0500

    Add sequence lookup file downloading.

--- acs/download_summary_files.py
+++ acs/download_summary_files.py
@@ -3,14 +3,25 @@ import os
 import subprocess
 import glob
 
-FILE_URL_TEMPLATE = ("https://www2.census.gov/programs-surveys/acs/"
-                     "summary_file/{}/data/5_year_by_state/"
-                     "Pennsylvania_Tracts_Block_Groups_Only.zip")
+SUMMARY_FILE_URL_TEMPLATE = ("https://www2.census.gov/programs-surveys/acs/"
+                             "summary_file/{}/data/5_year_by_state/"
+                             "Pennsylvania_Tracts_Block_Groups_Only.zip")
+
+# URL for the sequence lookup file
+SEQUENCE_FILE_URL_TEMPLATE = ("https://www2.census.gov/programs-surveys/acs/"
+                              "summary_file/{}/documentation/5_year/user_tools"
+                              "/Sequence_Number_and_Table_Number_Lookup.txt")
+
+SEQUENCE_FILENAME = 'sequence_lookup.txt'
 
 YEARS = range(2009, 2013)
 
 def summary_file_url(year):
-    return FILE_URL_TEMPLATE.format(year)
+    return SUMMARY_FILE_URL_TEMPLATE.format(year)
+
+
+def sequence_file_url(year):
+    return SEQUENCE_FILE_URL_TEMPLATE.format(year)
 
 
 def year_dir(year):
@@ -40,11 +51,31 @@ def unzip(filename):
     os.remove(filename)
 
 
-def clean(year):
+def download_sequence_file(year):
+    dirname = year_dir(year)
+    filename = os.path.join(dirname, SEQUENCE_FILENAME)
+    url = sequence_file_url(year)
+    subprocess.run(['curl', '-o', filename, url], check=True)
+    return filename
+
+
+def remove_margin_of_error_files(year):
     dirname = year_dir(year)
     margin_error_files = glob.glob(os.path.join(dirname, 'm*.txt'))
     for f in margin_error_files:
         os.remove(f)
+
+
+def fix_encoding(filename):
+    """
+    Sequence lookup files are encoded as latin1 with weird characters.
+
+    This converts the file to utf8 using the iconv utility.
+    """
+    temp_f = filename + '.tmp'
+    subprocess.run(['iconv', '-t', 'utf-8', '-c', '-o', temp_f, filename],
+                   check=True)
+    os.rename(temp_f, filename)
 
 
 if __name__ == '__main__':
@@ -60,6 +91,8 @@ if __name__ == '__main__':
 
     print('Downloading summary files. This might take a while...')
     for year in YEARS:
-        zip_filename = download_zip(year)
-        unzip(zip_filename)
-        clean(year)
+        #zip_filename = download_zip(year)
+        #unzip(zip_filename)
+        #remove_margin_of_error_files(year)
+        sequence_filename = download_sequence_file(year)
+        fix_encoding(sequence_filename)
