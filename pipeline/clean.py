63661dd68f3ce1f9f9a1680fb443e2fcbbaa563b
alliecollins <allisonleecollins@gmail.com>
2019-05-31 14:37:25 -0500

    committing clean file and latest merged df

--- pipeline/clean.py
+++ pipeline/clean.py
@@ -9,47 +9,46 @@ def clean_overall_data(complete_df):
     '''
     Execute cleaning steps prior to splitting into test and train
     '''
-    df = clean_missing_vals(complete_df)
-    df = clean_period_as_missing(df)
+    df = clean_period_as_missing(complete_df)
     df = convert_to_numeric(df, ['renter_occupied_household_size','median_gross_rent',
         'median_household_income'])
+    df = clean_missing_vals(df)
     df = impute_as_zero(df, ['violations_count','crime_count'])
 
     return df
 
 ####### FUNCTIONS TO RUN TO FIT ON TRAIN DATA ONLY ######
 
-def create_scaler(train_df, cols):
-    '''
-    Creates scalers for relevant columns in a dataframe
-    '''
+def get_feature_generators(train_df):
+    feature_generator_dict = {}
 
-    scaler_dict = {}
+    feature_generator_dict['scalers'] = create_scaler(train_df,['median_household_income'])
+    feature_generator_dict['binaries'] = get_medians(train_df,['median_gross_rent'])
 
-    for col in cols:
-        scaler = preprocessing.StandardScaler()
-        scaler.fit(train_df[[col]])
-        scaler_dict[col] = scaler
+    return feature_generator_dict
 
-    return scaler_dict
 
 ####### FUNCTIONS TO RUN ON BOTH TRAIN AND TEST SEPARATELY ######
 
-def clean_and_create_features(train_df, test_df, scaler=None):
+def clean_and_create_features(train_df, test_df, feature_generator_dict=None):
 
     #Impute missing values
-    test_df = simple_imputation(test_df, ['black_alone_owner_occupied','for_rent',
+    test_df = simple_imputation(test_df, ['black_alone_owner_occupied','for_rent_units',
         'median_gross_rent', 'median_household_income', 'num_af_am_alone', 'num_hisp',
         'num_unemployed', 'num_with_ged', 'num_with_high_school_degree',
-        'occupied', 'renter_occupied_household_size', 'units'])
+        'occupied_units', 'renter_occupied_household_size', 'units','vacant_units'])
 
-    train_df = simple_imputation(test_df, ['black_alone_owner_occupied','for_rent',
+    train_df = simple_imputation(test_df, ['black_alone_owner_occupied','for_rent_units',
         'median_gross_rent', 'median_household_income', 'num_af_am_alone', 'num_hisp',
         'num_unemployed', 'num_with_ged', 'num_with_high_school_degree',
-         'occupied', 'renter_occupied_household_size', 'units'])
+         'occupied_units', 'renter_occupied_household_size', 'units','vacant_units'])
 
-    if scaler:
-        train_df, test_df = scale_data(train_df, test_df, scaler)
+    if feature_generator_dict:
+        scalers = feature_generator_dict['scalers']
+        binaries = feature_generator_dict['binaries']
+
+        train_df, test_df = scale_data(train_df, test_df, scalers)
+        train_df, test_df = binarize_data(train_df, test_df, binaries)
 
     return train_df, test_df
 
@@ -108,12 +107,47 @@ def scale_data(train_df, test_df, scaler_dict):
     '''
     for col, scaler in scaler_dict.items():
         
-        #Fit the columns
-        train_df[col] = scaler.transform(train_df[[col]])
-        test_df[col] = scaler.transform(test_df[[col]])
-
-        #Clamp the testing data
-        test_df[col] = np.where(test_df[col] < 0, 0, test_df[col])
-        test_df[col] = np.where(test_df[col] > 1, 1, test_df[col])
+        train_df[col+'_scaled'] = scaler.transform(train_df[[col]])
+        test_df[col+'_scaled'] = scaler.transform(test_df[[col]])
 
     return train_df, test_df
+
+def create_scaler(train_df, cols):
+    '''
+    Creates scalers for relevant columns in a dataframe
+    '''
+
+    scaler_dict = {}
+
+    for col in cols:
+        scaler = preprocessing.StandardScaler()
+        scaler = scaler.fit(train_df[[col]])
+        scaler_dict[col] = scaler
+
+    return scaler_dict
+
+def get_medians(train_df, cols):
+    '''
+    Create dict of cols and median vals in training data to apply on train + test in
+    creating binary columns
+    '''
+    median_dict = {}
+
+    for col in cols:
+        median_dict[col] = train_df[col].median()
+
+    return median_dict
+
+def binarize_data(train_df, test_df, median_dict):
+    '''
+    Takes median dictionaries
+    '''
+    for col, median in median_dict.items():
+        train_df[col+'_binary'] = np.where(train_df[col] >= median, 1, 0)
+        test_df[col+'_binary'] = np.where(test_df[col] >= median, 1, 0)
+
+    return train_df, test_df
+
+
+
+
