7b00256003ba452be3d4c64a46f07291a4c1b38e
Sinclair Target <sinclairtarget@gmail.com>
2019-06-03 18:00:39 -0500

    Add function to detect NaN and Inf.

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -4,4 +4,5 @@ from .loop import *
 from .clean import *
 from .label import *
 from .bias_cop import BiasCop
+from .explore import *
 from . import notebook
---
+++ pipeline/explore.py
@@ -0,0 +1,12 @@
+import numpy as pd
+import pandas as pd
+
+def check_any_nan_or_inf(df, name='df'):
+    """
+    Checks a dataframe to see if it contains NaN or infinity values.
+    """
+    for colname in df.columns:
+        if np.isnan(df[colname]).any():
+            raise Exception(f"Found a NaN in column {colname} in {name}.")
+        elif not np.isfinite(df[colname].values).all():
+            raise Exception(f"Found an Inf in column {colname} in {name}.")
