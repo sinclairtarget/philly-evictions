fc42105f237113d9051da043c708d9221ce7e434
Sinclair Target <sinclairtarget@gmail.com>
2019-06-03 18:16:58 -0500

    Add ratio to nan checker function.

--- pipeline/explore.py
+++ pipeline/explore.py
@@ -1,12 +1,18 @@
-import numpy as pd
+import numpy as np
 import pandas as pd
 
 def check_any_nan_or_inf(df, name='df'):
     """
     Checks a dataframe to see if it contains NaN or infinity values.
     """
+    total = len(df)
+
     for colname in df.columns:
         if np.isnan(df[colname]).any():
-            raise Exception(f"Found a NaN in column {colname} in {name}.")
+            count = sum(np.isnan(df[colname]))
+            ratio = count / total
+            raise Exception(f"Found NaN in column {colname} in {name} ({ratio}).")
         elif not np.isfinite(df[colname].values).all():
-            raise Exception(f"Found an Inf in column {colname} in {name}.")
+            count = sum(np.isfinite(df[colname]))
+            ratio = (total - count) / total
+            raise Exception(f"Found Inf in column {colname} in {name} ({ratio}).")
