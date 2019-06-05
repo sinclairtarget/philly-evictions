3873a8d217b71a10312be740a6420423de3d9ac7
Sinclair Target <sinclairtarget@gmail.com>
2019-06-05 12:45:39 -0500

    Update bias cop to use percentages.

--- pipeline/bias_cop.py
+++ pipeline/bias_cop.py
@@ -10,12 +10,18 @@ class BiasCop:
 
     Generates "fairness" groups based on the majority demographic in a
     neighborhood.
+
+    Expects to be given a "results_df" that has a label column, a (binary)
+    score column, and demographic columns. These demographic columsn should be
+    the percentage of people in a block group that belong to the demographic.
     """
     DEFAULT_CONFIG = {
         'label_col': 'label',
         'score_col': 'score',
-        'population_col': 'total_population',
-        'demographic_features': ['num_af_am_alone', 'num_hisp']
+        'demographics': {
+            'af_am_alone': 'num_af_am_alone_percent',
+            'hisp': 'num_hisp_percent'
+        }
     }
 
     def __init__(self, results_df, **kwargs):
@@ -51,20 +57,23 @@ class BiasCop:
     def _preprocess(self, df):
         df = self._create_groups(df)
 
-        # Temp hack for now
         score_col = self.config['score_col']
-        df[score_col] = 1
-
-        # Aequitas wants the label column to be called by this name
         label_col = self.config['label_col']
-        df['label_value'] = df[label_col]
-        df = df.drop(columns=[label_col])
+
+        # Rename columns to use Aequitas' names
+        if label_col != 'label_value':
+            df = df.assign(label_value=df[label_col])
+            df = df.drop(columns=[label_col])
+
+        if score_col != 'score':
+            df = df.assign(score=df[score_col])
+            df = df.drop(columns=[score_col])
 
         # Make sure group col is a string
         df.majority_demo = df.majority_demo.astype(str)
 
         # Filter to only what Aequitas needs
-        columns = ['label_value', score_col, 'majority_demo']
+        columns = ['label_value', 'score', 'majority_demo']
         df, _ = preprocess_input_df(df[columns])
 
         return df
@@ -76,14 +85,9 @@ class BiasCop:
 
 
     def _majority(self, df_row):
-        pop_col = self.config['population_col']
-
-        for demo in self.config['demographic_features']:
-            # If we don't have the data, we might miss some majority demos :(
-            demo_count = self._filter_nan(df_row[demo])
-            total_pop = self._filter_nan(df_row[pop_col], fallback=demo_count)
-
-            if (total_pop > 0 and (demo_count / total_pop) > 0.5):
+        for demo, metric in self.config['demographics'].items():
+            demo_perc = self._filter_nan(df_row[metric])
+            if demo_perc > 0.5:
                 return demo
 
         return 'other'
