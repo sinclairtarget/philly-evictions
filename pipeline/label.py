2b9fecc762f0478f70bc4efc4b374c89a507c62f
Sinclair Target <sinclairtarget@gmail.com>
2019-06-05 12:45:21 -0500

    Add label_df method to pipeline.

--- pipeline/label.py
+++ pipeline/label.py
@@ -1,18 +1,43 @@
 import numpy as np
 import pandas as pd
 
-def label(split, lower_bound, drop_column=False):
-    train_df, test_df = split
-    train_df = train_df.copy() # Err, think about why this is needed later
-    test_df = test_df.copy()
+DEFAULTS = {
+    'label_col': 'label',
+    'evictions_col': 'evictions',
+    'drop_column': True
+}
 
-    train_df['label'] = \
-            np.where(train_df['evictions'] > lower_bound, 1, 0)
-    test_df['label'] = \
-            np.where(test_df['evictions'] > lower_bound, 1, 0)
+def label(split, lower_bound, **kwargs):
+    """
+    Labels a train/test df pair.
+    """
+    train_df, test_df = split
+    train_df = label_df(train_df, lower_bound, **kwargs)
+    test_df = label_df(test_df, lower_bound, **kwargs)
+    return train_df, test_df
+
+
+def label_df(df, lower_bound, **kwargs):
+    """
+    Labels rows that have evictions above the given lower bound as a 1,
+    while rows below that bound get a 0.
+    """
+    settings = _settings(kwargs)
+    label_col = settings['label_col']
+    evictions_col = settings['evictions_col']
+    drop_column = settings['drop_column']
+
+    df = df.copy() # Err, think about why this is needed later
+
+    df[label_col] = np.where(df[evictions_col] > lower_bound, 1, 0)
 
     if drop_column:
-        train_df = train_df.drop(columns=['evictions'])
-        test_df = test_df.drop(columns=['evictions'])
+        df = df.drop(columns=[evictions_col])
 
-    return train_df, test_df
+    return df
+
+
+def _settings(kwargs):
+    settings = DEFAULTS.copy()
+    settings.update(kwargs)
+    return settings
