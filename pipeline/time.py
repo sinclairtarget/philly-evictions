8baca265258d7b0eedfc6f00d44b262dba2781c7
Sinclair Target <sinclairtarget@gmail.com>
2019-06-06 17:59:28 -0500

    split_by_year() now creates one-year training sets.

--- pipeline/time.py
+++ pipeline/time.py
@@ -5,16 +5,17 @@ def split_by_year(df, colname='year', drop_col=False):
     Returns a list of two-tuples where the first element in the tuple is a
     training set dataframe and the second element is the corresponding test set
     dataframe. There is a test set for each year except the first. The training
-    set consists of all years before the test set.
+    set is only ever the previous year.
 
     For example, given a dataframe with data for 2012, 2013, 2014, and 2015
     this function returns dataframes for the years:
 
-    [(2012, 2013), (2012-2013, 2014), (2012-2014, 2015)]
+    [(2012, 2013), (2013, 2014), (2014, 2015)]
     """
     min_year = df[colname].min()
-    test_years = range(min_year + 1, df[colname].max() + 1)
-    year_tuples = [(range(min_year, year), year) for year in test_years]
+    max_year = df[colname].max()
+    years = range(min_year, max_year + 1)
+    year_tuples = zip(years[:-1], years[1:])
     return [_df_tuple(df, colname, drop_col, year_tuple)
             for year_tuple in year_tuples]
 
@@ -44,8 +45,8 @@ def split_boundaries(splits, colname='year'):
 
 
 def _df_tuple(df, colname, drop_col, year_tuple):
-    train_range, test_year = year_tuple
-    train_df = df[df[colname].isin(train_range)]
+    train_year, test_year = year_tuple
+    train_df = df[df[colname] == train_year]
     test_df = df[df[colname] == test_year]
 
     if drop_col:
