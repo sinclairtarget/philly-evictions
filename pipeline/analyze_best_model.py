e1e727865e411def935858e7c4928835a7797cab
Sinclair Target <sinclairtarget@gmail.com>
2019-06-07 17:19:39 -0500

    Make sure precision recall curve does not cut off thresholds array when recall is 1.

--- pipeline/analyze_best_model.py
+++ pipeline/analyze_best_model.py
@@ -1,50 +1,42 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import * 
+from sklearn.metrics import *
+from sklearn.metrics.ranking import _binary_clf_curve
 
 from sklearn.tree import DecisionTreeClassifier
 
-def plot_precision_recall_n(model, params, train_df, test_df, label):
-    X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
-    model.set_params(**params)
-    model.fit(X_train, y_train)
-    pred_scores = model.predict_proba(X_test)[:,1]
+def plot_precision_recall_n(scored_df):
+    y_true = scored_df.label
+    y_score = scored_df.score
+    precision_curve, recall_curve, pr_thresholds = \
+        _precision_recall_curve_no_truncate(y_true, y_score)
 
-    y_true = y_test
-    y_prob = pred_scores
-
-    print(y_true.mean())
-
-    from sklearn.metrics import precision_recall_curve
-    y_score = y_prob
-    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_score)
-    precision_curve = precision_curve[:-1]
-    recall_curve = recall_curve[:-1]
-    pct_above_per_thresh = []
     number_scored = len(y_score)
+    pct_above_per_thresh = []
     for value in pr_thresholds:
         num_above_thresh = len(y_score[y_score>=value])
         pct_above_thresh = num_above_thresh / float(number_scored)
         pct_above_per_thresh.append(pct_above_thresh)
+
     pct_above_per_thresh = np.array(pct_above_per_thresh)
-    
+
     plt.clf()
     fig, ax1 = plt.subplots()
     ax1.plot(pct_above_per_thresh, precision_curve, 'b')
+    ax1.set_ylim([0,1])
     ax1.set_xlabel('percent of population')
     ax1.set_ylabel('precision', color='b')
+
     ax2 = ax1.twinx()
     ax2.plot(pct_above_per_thresh, recall_curve, 'r')
     ax2.set_ylabel('recall', color='r')
-    ax1.set_ylim([0,1])
-    ax1.set_xlim([0,1])
     ax2.set_ylim([0,1])
     ax2.set_xlim([0,1])
     plt.show()
 
 
-def top_k_blocks_clf(model, params, train_df, test_df, label, k): 
+def top_k_blocks_clf(model, params, train_df, test_df, label, k):
     X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
     model.set_params(**params)
     model.fit(X_train, y_train)
@@ -55,7 +47,7 @@ def top_k_blocks_clf(model, params, train_df, test_df, label, k):
     return top_16[['GEOID', 'label', 'pred_scores']]
 
 
-def top_k_blocks_reg(model, params, train_df, test_df, label, k): 
+def top_k_blocks_reg(model, params, train_df, test_df, label, k):
     X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
     model.set_params(**params)
     model.fit(X_train, y_train)
@@ -66,33 +58,45 @@ def top_k_blocks_reg(model, params, train_df, test_df, label, k):
     return top_16[['GEOID', 'evictions', 'preds']]
 
 
-def feature_importance_clf(model, params, train_df, test_df, label): 
+def feature_importance_clf(model, params, train_df, test_df, label):
     X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
     model.set_params(**params)
-    model.fit(X_train, y_train)    
-    feature_importance = pd.DataFrame(zip(X_test.columns, model.feature_importances_), 
+    model.fit(X_train, y_train)
+    feature_importance = pd.DataFrame(zip(X_test.columns, model.feature_importances_),
                                       columns=['feature', 'importance'])
     feature_importance.sort_values(by='importance', ascending=False, inplace=True)
     return feature_importance
 
 
-def feature_importance_reg(model, params, train_df, test_df, label): 
+def feature_importance_reg(model, params, train_df, test_df, label):
     X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
     model.set_params(**params)
-    model.fit(X_train, y_train)  
+    model.fit(X_train, y_train)
 
-    feature_importance = pd.DataFrame(zip(X_test.columns, model.coef_), 
+    feature_importance = pd.DataFrame(zip(X_test.columns, model.coef_),
                                       columns=['feature', 'coef'])
     feature_importance['absv_coef'] = feature_importance['coef'].abs()
     feature_importance.sort_values(by='absv_coef', ascending=False, inplace=True)
     return feature_importance
 
 
-def split_dfs(train_df, test_df, label): 
+def split_dfs(train_df, test_df, label):
     X_train = train_df.drop(columns=['GEOID', 'year_evictions', label])
     X_test = test_df.drop(columns=['GEOID', 'year_evictions', label])
     y_train = train_df[label]
     y_test = test_df[label]
-    return X_train, X_test, y_train, y_test     
+    return X_train, X_test, y_train, y_test
 
 
+def _precision_recall_curve_no_truncate(y_true, y_score):
+    """
+    Reimplementation of sklearn precision_recall_curve function that doesn't
+    truncate thresholds array once recall is 1.
+    """
+    fps, tps, thresholds = _binary_clf_curve(y_true, y_score)
+
+    precision = tps / (tps + fps)
+    precision[np.isnan(precision)] = 0
+    recall = tps / tps[-1]
+
+    return precision, recall, thresholds
