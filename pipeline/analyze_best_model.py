b52b6691d52434abc02b4dd58490b5263a602d0c
Erika Tyagi <erika.tyagi@gmail.com>
2019-06-09 00:11:44 -0500

    lots of output

--- pipeline/analyze_best_model.py
+++ pipeline/analyze_best_model.py
@@ -3,92 +3,85 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import *
 from sklearn.metrics.ranking import _binary_clf_curve
+from functools import reduce
 
-from sklearn.tree import DecisionTreeClassifier
 
-def plot_precision_recall_n(scored_df):
+def feature_importance(model, features): 
+    """
+    Returns sorted df with feature names and their importance. 
+    """
+    all_vars = list(features)
+    features = [v for v in all_vars if v not in (
+        'GEOID', 'year_evictions', 'evictions', 'label')]
+    feature_importance = pd.DataFrame(zip(features, model.feature_importances_),
+                                      columns=['feature', 'importance'])
+    feature_importance.sort_values(by='importance', ascending=False, inplace=True)
+    return feature_importance
+
+
+def select_k_blocks(df, k, sort_vars, output_vars): 
+    """
+    Returns a df with the top k percent of blocks (based on sort_vars). 
+    Only output_vars are returned. 
+    """
+    num_blocks = int(df.shape[0]*k)//1 
+    selected_blocks = df.nlargest(num_blocks, sort_vars)[output_vars]
+    return selected_blocks
+
+
+def clf_reg_comparison(clf, clf_scores, reg, reg_scores, test_df, k):
+    """
+    Returns a df comparing how the clf and reg models perform in predicting 
+    the top k percent of blocks in the test_df. Returns the top k percent of 
+    blocks in the test_df, along with that block's predicted score and 
+    predicted label (from the clf) and predicted evictions (from the reg). 
+    Blank predictions indicate that the clf/reg didn't predict that block 
+    group to be in the top k percent. 
+    """
+    test_blocks = select_k_blocks(test_df, k, ['evictions'], ['GEOID', 'evictions'])
+    clf_blocks = select_k_blocks(clf_scores, k, ['score'], ['GEOID', 'score', 'label'])
+    reg_blocks = select_k_blocks(reg_scores, k, ['pred_evictions'], ['GEOID', 'pred_evictions'])
+    merged_df = reduce(lambda left, right: pd.merge(
+        left, right, on='GEOID', how='left'), [test_blocks, clf_blocks, reg_blocks])
+    merged_df.rename(columns={
+        'evictions': 'actual_evictions', 
+        'score': 'clf_pred_score', 
+        'label': 'clf_pred_label', 
+        'pred_evictions': 'reg_pred_evictions'}, inplace=True)
+    merged_df.sort_values(by='actual_evictions', ascending=False, inplace=True)
+    return merged_df
+
+
+def plot_precision_recall_n(scored_df, filename):
+    """
+    Outputs a precision-recall curve (shows in notebook and saves to filename). 
+    """
     y_true = scored_df.label
     y_score = scored_df.score
     precision_curve, recall_curve, pr_thresholds = \
         _precision_recall_curve_no_truncate(y_true, y_score)
-
     number_scored = len(y_score)
     pct_above_per_thresh = []
     for value in pr_thresholds:
         num_above_thresh = len(y_score[y_score>=value])
         pct_above_thresh = num_above_thresh / float(number_scored)
         pct_above_per_thresh.append(pct_above_thresh)
-
     pct_above_per_thresh = np.array(pct_above_per_thresh)
-    print(precision_curve[-1])
-    print(recall_curve[-1])
 
     plt.clf()
     fig, ax1 = plt.subplots()
     ax1.plot(pct_above_per_thresh, precision_curve, 'b')
-
     margin = 0.01
     ax1.set_ylim([0-margin,1+margin])
     ax1.set_xlabel('percent of population')
     ax1.set_ylabel('precision', color='b')
-
     ax2 = ax1.twinx()
     ax2.plot(pct_above_per_thresh, recall_curve, 'r')
     ax2.set_ylabel('recall', color='r')
     ax2.set_ylim([0-margin,1+margin])
     ax2.set_xlim([0-margin,1+margin])
+    plt.savefig(filename)
     plt.show()
-
-
-def top_k_blocks_clf(model, params, train_df, test_df, label, k):
-    X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
-    model.set_params(**params)
-    model.fit(X_train, y_train)
-    pred_scores = model.predict_proba(X_test)[:,1]
-    test_df['pred_scores'] = pred_scores
-    test_df.sort_values(by='pred_scores', ascending=False, inplace=True)
-    top_16 = test_df.head(int(test_df.shape[0]*k)//1)
-    return top_16[['GEOID', 'label', 'pred_scores']]
-
-
-def top_k_blocks_reg(model, params, train_df, test_df, label, k):
-    X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
-    model.set_params(**params)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-    test_df['preds'] = preds
-    test_df.sort_values(by='preds', ascending=False, inplace=True)
-    top_16 = test_df.head(int(test_df.shape[0]*k)//1)
-    return top_16[['GEOID', 'evictions', 'preds']]
-
-
-def feature_importance_clf(model, params, train_df, test_df, label):
-    X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
-    model.set_params(**params)
-    model.fit(X_train, y_train)
-    feature_importance = pd.DataFrame(zip(X_test.columns, model.feature_importances_),
-                                      columns=['feature', 'importance'])
-    feature_importance.sort_values(by='importance', ascending=False, inplace=True)
-    return feature_importance
-
-
-def feature_importance_reg(model, params, train_df, test_df, label):
-    X_train, X_test, y_train, y_test = split_dfs(train_df, test_df, label)
-    model.set_params(**params)
-    model.fit(X_train, y_train)
-    feature_importance = pd.DataFrame(zip(X_test.columns, model.coef_),
-                                      columns=['feature', 'coef'])
-    feature_importance['absv_coef'] = feature_importance['coef'].abs()
-    feature_importance.sort_values(by='absv_coef', ascending=False, inplace=True)
-    return feature_importance
-
-
-def split_dfs(train_df, test_df, label):
-    X_train = train_df.drop(columns=['GEOID', 'year_evictions', label])
-    X_test = test_df.drop(columns=['GEOID', 'year_evictions', label])
-    y_train = train_df[label]
-    y_test = test_df[label]
-    return X_train, X_test, y_train, y_test
 
 
 def _precision_recall_curve_no_truncate(y_true, y_score):
