598e933daa5d050a5d71927ac5fece4f4d58ec57
Erika Tyagi <erika.tyagi@gmail.com>
2019-06-12 14:54:21 -0500

    final versions

--- plot-map.r
+++ plot-map.r
@@ -2,7 +2,7 @@ library('tidyverse')
 library('sf')
 
 # Spatially join selected blocks and block groups 
-df <- read_csv('results/2011/clf_selected_blocks.csv')
+df <- read_csv('results/2014/clf_selected_blocks.csv')
 df.geo <- st_read('data/block-groups.geojson')
 df <- df %>% mutate(GEOID = factor(GEOID), label = factor(label))
 df.geo <- df.geo %>% filter(grepl('^42101', GEOID))
@@ -11,6 +11,6 @@ df <- left_join(df.geo, df)
 # Create map 
 ggplot(df) +
     geom_sf(aes(fill = label)) +
-    ggtitle("Best Classifier: Top 14% Predictions - 2011")
-ggsave('results/2011/map.png', dpi=150)
+    ggtitle("Best Classifier: Top 14% Predictions - 2014")
+ggsave('results/2014/map.png', dpi=150)
 
