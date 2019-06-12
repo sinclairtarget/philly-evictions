fd4e025b78e63a18245f1e415b1293faf73270a6
Sinclair Target <sinclairtarget@gmail.com>
2019-06-11 23:10:09 -0400

    Remove old tasks.

--- dodo.py
+++ dodo.py
@@ -1,51 +1,5 @@
 from tasks import *
 
 DOIT_CONFIG = {
-    'default_tasks': ['render_proposal']
+    'default_tasks': ['evaluate']
 }
-
-def task_render_proposal():
-    """Generates HTML notebook output for proposal."""
-    target = 'proposal/proposal.html'
-    dep = 'proposal/proposal.ipynb'
-    return {
-        'file_dep': [dep],
-        'targets': [target],
-        'actions': [
-            f"jupyter nbconvert --execute --to html {dep}"
-        ],
-        'clean': True
-    }
-
-
-def task_sync_proposal():
-    """Generate .ipynb notebook from proposal markdown source."""
-    target = 'proposal/proposal.ipynb'
-    dep = 'proposal/proposal.md'
-    return {
-        'file_dep': [dep],
-        'targets': [target],
-        'actions': [
-            f"jupytext --to notebook {dep}",
-            f"jupytext --set-format ipynb,md --sync {target}"
-        ],
-        'clean': True
-    }
-
-
-def task_sync_analysis():
-    """Generate .ipynb notebook from analysis markdown source."""
-    target = 'analysis.ipynb'
-    dep = 'analysis.md'
-    return {
-        'file_dep': [dep],
-        'targets': [target],
-        'actions': [
-            f"jupytext --to notebook {dep}",
-            f"jupytext --set-format ipynb,md --sync {target}"
-        ],
-        'clean': True
-    }
-
-
-
